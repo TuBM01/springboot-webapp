25ac0f718741eb6381572585d8e3af703251fc0e
tubm03 <buiminhtu12112003@gmail.com>
2026-01-25 22:16:58 +0700

    add CORS

--- backend/src/main/java/com/tubm/webapp/backend/controller/ProductController.java
+++ backend/src/main/java/com/tubm/webapp/backend/controller/ProductController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin // CORS: cross-origin resource sharing: allow requests from different origins
+// @CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
     @Autowired
