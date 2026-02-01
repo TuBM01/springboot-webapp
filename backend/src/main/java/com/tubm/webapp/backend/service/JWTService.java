f3b88310b17acccd3d0e55c341169b508ff83150
tubm03 <buiminhtu12112003@gmail.com>
2026-02-01 20:02:59 +0700

    Generating JWT token

--- backend/src/main/java/com/tubm/webapp/backend/service/JWTService.java
+++ backend/src/main/java/com/tubm/webapp/backend/service/JWTService.java
@@ -2,10 +2,52 @@ package com.tubm.webapp.backend.service;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Jwts;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import java.security.Key;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.io.Decoders;
+import java.util.Base64;
+
 @Service
 public class JWTService {
 
+    private String secretKey;
+
+    public JWTService() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGenerator.generateKey();
+            this.secretKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public String generateToken(String username) {
-        return "token";
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .and()
+                .signWith(getKey())
+                .compact();
+
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
